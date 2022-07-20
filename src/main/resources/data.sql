42151363c277e90cee13d2dbc55b99bf69d160b8
Даниил Лытин <d.lytin@groupstp.ru>
2022-07-20 11:41:10 +0800

    added H2 as database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into PROJECT (ID, NAME) values (1, 'demo');
+insert into TASK (ID, DESCRIPTION, PROJECT_ID) values (1, 'code', 1);
+insert into TASK (ID, DESCRIPTION, PROJECT_ID) values (2, 'deploy', 1);
