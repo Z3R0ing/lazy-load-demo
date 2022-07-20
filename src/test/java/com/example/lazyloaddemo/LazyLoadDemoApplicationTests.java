405c5ae537c95e9704b486d73e01fe26b142a129
Даниил Лытин <d.lytin@groupstp.ru>
2022-07-20 11:38:31 +0800

    init commit

---
+++ src/test/java/com/example/lazyloaddemo/LazyLoadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lazyloaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyLoadDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
