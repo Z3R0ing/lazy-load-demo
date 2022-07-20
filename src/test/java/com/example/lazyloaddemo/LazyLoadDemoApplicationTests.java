138d0912642f645c3e2dfab95f439e4cb31c9b3c
Даниил Лытин <d.lytin@groupstp.ru>
2022-07-20 11:42:48 +0800

    added lazyLoad test

--- src/test/java/com/example/lazyloaddemo/LazyLoadDemoApplicationTests.java
+++ src/test/java/com/example/lazyloaddemo/LazyLoadDemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.lazyloaddemo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class LazyLoadDemoApplicationTests {
 
+	@Autowired
+	ProjectRepository projectRepository;
+
 	@Test
-	void contextLoads() {
+	@Transactional
+	void lazyLoad() {
+		Project project = projectRepository.findById(1L).orElse(null);
+		Assertions.assertNotNull(project);
+		System.err.println(project);
+		List<Task> tasks = project.getTasks();
+		Assertions.assertFalse(tasks.isEmpty());
+		System.err.println(tasks);
 	}
 
 }
